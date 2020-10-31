bdd998dc4cf40509de0f89efc92705394c6b9b4e
eabayev20 <eabayev20@gmail.com>
2020-10-30 23:10:55 -0400

    almsot done, now the bulk of testing

--- SuperArray.java
+++ SuperArray.java
@@ -8,12 +8,27 @@ public class SuperArray {
     return data.length;
   }
   public boolean add(String element) {
+    if (size() >= size) {
+      resize();
+    }
     int pos = size();
     data[pos] = element;
     return true;
   }
   public String get(int index) {
     return data[index];
+  }
+  public String set (int index, String element) {
+    String temp = "";
+    temp = data[index];
+    data[index] = element;
+    return temp;
+  }
+  private void resize(){
+    String [] data2;
+    data2 = new String [size + 10];
+    data2 = data;
+    data = data2;
   }
 
 
